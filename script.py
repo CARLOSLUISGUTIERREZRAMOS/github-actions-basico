b1a840939d9340ba6ed52ceab900182633a2f9ab
CLGR <carlos5t@hotmail.com>
2026-04-25 14:16:28 -0500

    Add MENSAJE environment variable and print statement
    Added a new environment variable 'MENSAJE' and print statement to display its value.

--- script.py
+++ script.py
@@ -2,8 +2,10 @@ import os
 
 entorno = os.getenv("APP_ENV")
 api_key = os.getenv("API_KEY")
+mensaje = os.getenv("MENSAJE")
 
 print("✅ Script ejecutado correctamente")
+print(f"📝 Mensaje recibido: {mensaje}")
 print(f"🌍 Entorno: {entorno}")
 
 if api_key:
